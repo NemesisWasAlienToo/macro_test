1de58e4183b2346fff36d336b0107d2a5462896c
Nemesis <kian.khaje@gmail.com>
2023-08-10 02:00:00 +0200

    Double quote hanlded

--- examples/quote.cpp
+++ examples/quote.cpp
@@ -10,6 +10,7 @@ struct String
     constexpr String() = default;
     constexpr String(String const &Other) { std::copy_n(Other.data, N, data); }
     constexpr String(char const (&str)[N + 1]) { std::copy_n(str, N, data); }
+    constexpr auto operator[](std::size_t i) const { return data[i]; }
 };
 
 template <>
@@ -21,6 +22,102 @@ struct String<0>
     constexpr String(String const &) = default;
     constexpr String(char const (&)[1]) {}
 };
+
+template <String Original, String Text>
+constexpr auto Find(size_t Index)
+{
+    for (size_t i = Index; i < Original.size - Text.size + 1; i++)
+    {
+        if (Original[i] != Text[0])
+            continue;
+
+        bool found = true;
+
+        for (size_t j = 1; j < Text.size; j++)
+        {
+            if (Original[i + j] != Text[j])
+            {
+                found = false;
+                break;
+            }
+        }
+
+        if (found)
+        {
+            return i;
+        }
+    }
+
+    return static_cast<size_t>(-1);
+}
+
+template <String Original, String Phrase>
+constexpr size_t FindCount()
+{
+    if (!Phrase.size || !Original.size)
+        return 0;
+
+    size_t Count = 0;
+    size_t Index = 0;
+
+    while ((Index = Find<Original, Phrase>(Index)) != static_cast<size_t>(-1))
+    {
+        Count++;
+        Index += Phrase.size;
+    }
+
+    return Count;
+}
+
+template <String Original, String Phrase, String Text>
+constexpr auto Replace(size_t Index = 0)
+{
+    constexpr size_t DataSize = []()
+    {
+        if (Phrase.size == Text.size)
+            return Original.size;
+        
+        size_t Count = FindCount<Original, Phrase>();
+
+        if (Text.size > Phrase.size)
+        {
+            return Original.size + Count * (Text.size - Phrase.size);
+        }
+        else
+        {
+            return Original.size - Count * (Phrase.size - Text.size);
+        }
+    }();
+
+    char Data[DataSize + 1];
+
+    if (!Original.size || !Phrase.size)
+        return String(Data);
+
+    size_t OCounter = 0, RCounter = 0;
+
+    while ((Index = Find<Original, Phrase>(RCounter)) != static_cast<size_t>(-1))
+    {
+        for (; RCounter < Index; RCounter++)
+        {
+            Data[OCounter++] = Original[RCounter];
+        }
+
+        for (size_t i = 0; i < Text.size; i++)
+        {
+            Data[OCounter++] = Text[i];
+        }
+
+        RCounter += Phrase.size;
+    }
+
+    for (; RCounter < Original.size; RCounter++)
+    {
+        Data[OCounter++] = Original[RCounter];
+    }
+
+    return String(Data);
+}
 
 template <std::size_t N>
 String(char const (&)[N]) -> String<N - 1>;
@@ -50,10 +147,10 @@ constexpr auto quotes()
 template <String... Tokens>
 struct quoteImpl
 {
-  static constexpr auto result = quotes<concat<Tokens...>()>();
+  static constexpr auto result = quotes<Replace<concat<Tokens...>(), "\"", "\\\"">()>();
 };
 
-namespace std
+namespace std 
 {
   template<String... Tokens>
   constexpr auto quote()
@@ -62,9 +159,16 @@ namespace std
   }
 }
 
+template<String... Tokens>
+constexpr auto rec()
+{
+  //return macro std::quote{std::cout << \"hi\" << std::endl};
+  return macro std::quote{std::cout << "hi" << std::endl};
+}
+
 int main()
 {
-    std::cout << macro std::quote{hi there};
-   
-    return 0;
+  macro rec{};
+  //std::cout << macro std::macros::quote{hi there} << std::endl;
+  return 0;
 }
